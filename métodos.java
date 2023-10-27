6cfcdd72689b2b69e732bc1cd3563406d03cefd0
FuraPneu <130803560+FuraPneu@users.noreply.github.com>
2023-10-27 10:55:47 -0300

    Update métodos.java

--- métodos.java
+++ métodos.java
@@ -7,12 +7,12 @@ public class Pessoa {
     public String RG
     public Date data_de_nascimento;
 
-public Pessoa(String _nome, String _cpf, String_endereco, String _RG,
-    this.nome = _nome;
-    this.cpf = _cpf;
-    this.endereco = _endereco;
-    this.RG = _RG;
-    this.data_de_nascimento = _data;
+public Pessoa(String nome, String cpf, String endereco, String RG,
+    this.nome = nome;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.RG = RG;
+    this.data_de_nascimento = data;
     )
 
     public void alteraEndereco(String novoEndereco) {
